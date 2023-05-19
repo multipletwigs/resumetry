32a1121287b5a1ec30b18e67650742c228ab3ec0
Zach Khong <zachkhong.workplace@gmail.com>
2023-05-19 18:08:24 +0800

    init files

---
+++ frontend/app/providers.tsx
@@ -0,0 +1,15 @@
+// app/providers.tsx
+'use client'
+import { ChakraProvider } from '@chakra-ui/react'
+
+export function Providers({ 
+    children 
+  }: { 
+  children: React.ReactNode 
+  }) {
+  return (
+    <ChakraProvider>
+      {children}
+    </ChakraProvider>
+  )
+}
