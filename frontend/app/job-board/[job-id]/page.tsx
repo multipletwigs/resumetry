2f8c3b22c304f6d3b818a40f33d250034c062b6a
Zach Khong <zachkhong.workplace@gmail.com>
2023-05-20 00:35:46 +0800

    filter

--- frontend/app/job-board/[job-id]/page.tsx
+++ frontend/app/job-board/[job-id]/page.tsx
@@ -7,14 +7,18 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const params = useParams();
-  const [jobItem, setJobItem] = useState<CompanyJobOpening | undefined>(undefined)
+  const [jobItem, setJobItem] = useState<CompanyJobOpening | undefined>(
+    undefined
+  );
 
-  useEffect(()=>{
-    if (params.jobId) {
-      const jobItem = COMPANIES.flatMap((company) => company.jobOpenings).find((job) => job.jobId === Number(params.jobId));
+  useEffect(() => {
+    if (jobItem === undefined) {
+      const jobItem = COMPANIES.flatMap((company) => company.jobOpenings).find(
+        (job) => job.jobId === Number(params["job-id"])
+      );
       setJobItem(jobItem);
     }
-  }, [])
+  }, [jobItem]);
 
   return (
     // Centered container
@@ -26,8 +30,12 @@ export default function Home() {
       gap="5"
     >
       <Container maxW="700px" centerContent gap="5">
-        <Text fontSize={"2xl"} fontWeight={"700"}>Hi Hackoyaki</Text>
-        <Tag size="lg" variant="subtle" colorScheme="green">Hi there guys! Lappy here hehe {jobItem?.jobTitle}</Tag>
+        <Text fontSize={"2xl"} fontWeight={"700"}>
+          {jobItem?.jobTitle}
+        </Text>
+        <Tag size="lg" variant="subtle" colorScheme="green">
+          Hi there guys! Lappy here hehe {jobItem?.jobTitle}
+        </Tag>
         <Input placeholder="Type here..." size="lg" width="400px" />
       </Container>
     </Stack>
