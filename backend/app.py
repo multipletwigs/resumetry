a5509a963e2ef4060dd220532b67bd97590d9c72
xinle1030 <xinlelam1030@gmail.com>
2023-05-20 14:16:19 +0800

    add in CORS

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 import os
+from flask_cors import CORS
 
 # App Initialization
 from . import create_app, db # from __init__ file
 app = create_app(os.getenv("CONFIG_MODE")) 
+CORS(app,resources={r"/*":{"origins":"*"}})
 
 # Hello World!
 @app.route('/')
